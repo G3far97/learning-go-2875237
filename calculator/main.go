127c3a23dbf23764bae0bf95e1cb58b168046210
G3far97 <ahf1151@gmail.com>
2024-12-15 17:18:11 +0200

    write-read-local-files

--- calculator/main.go
+++ calculator/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("Files")
+	content := "Hello from Go!"
+	file, err := os.Create("./fromString.txt")
+	checkError(err)
+	length, err := io.WriteString(file, content)
+	checkError(err)
+	fmt.Printf("Wrote a file with %v charachters\n", length)
+	defer file.Close()
+	defer readFile("./fromString.txt")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readFile(fileName string) {
+	data, err := os.ReadFile(fileName)
+	checkError(err)
+	fmt.Println("Text read from file:", string(data))
 }
